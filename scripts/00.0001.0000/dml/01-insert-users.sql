6731da40a3016fb47e45ded4bbb30280b967459f
RubenFern <rubenferval@hotmail.com>
2025-07-22 18:14:06 +0200

    change in test users

--- scripts/00.0001.0000/dml/01-insert-users.sql
+++ scripts/00.0001.0000/dml/01-insert-users.sql
@@ -1,4 +1,3 @@
 INSERT INTO User (role_id, status_id, username, email, password)
 VALUES
-    (1, 1, 'alice', 'alice@example.com', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
-    (1, 1, 'bob',   'bob@example.com', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
+    (1, 1, 'test', 'test@test.com', '4fPb89GW3BeBm6QsrYBwH2k55TBZV96q399TSSLP/QWwBvu570aJAEGNuQujjsd/gF0LXGUxlJAd0p/foH3YzD3f93xDJai1tyUiqt5QhlLCb6cqXoSgHlN/wm1wFxDw');
